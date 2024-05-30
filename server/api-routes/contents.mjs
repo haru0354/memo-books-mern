dd5f25920e3cff8feec9a526229fa66fe6e032c6
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-30 13:03:03 +0900

    チャプターのコンテンツを追加するAPIの作成

--- server/api-routes/contents.mjs
+++ server/api-routes/contents.mjs
@@ -1,8 +1,9 @@
 import express from "express"
-import { getAllContents } from "../controllers/contentsController.mjs";
+import { addContents, getAllContents } from "../controllers/contentsController.mjs";
 
 const contentsRouter = express.Router();
 
 contentsRouter.get("/:id/:chapterId", getAllContents)
+contentsRouter.post("/:id/:chapterId", addContents)
 
 export default contentsRouter;
--- server/controllers/contentsController.mjs
+++ server/controllers/contentsController.mjs
@@ -18,3 +18,34 @@ export const getAllContents = async (req, res) => {
         res.status(500).json({ message: "コンテンツの取得に失敗しました。"})
     }
 }
+
+export const addContents = async (req, res) => {
+    try {
+        const bookId = req.params.id
+        const chapterId = req.params.chapterId
+    
+        const { chapter, book, error } = await findChapterById(bookId, chapterId)
+    
+        if (error) {
+            res.status(404).json({ message: error })
+        }
+    
+        const { heading_title, content } = req.body;
+    
+        if (!heading_title || !content) {
+          return res.status(400).json({ message: "heading_titleとcontentは必須です。" });
+        }
+    
+        const newContents = {
+          heading_title,
+          content,
+        };
+    
+        chapter.contents.push(newContents);
+        await book.save();
+        res.status(201).json(newContents)
+    } catch (err) {
+        console.error("コンテンツの追加に失敗しました。", err);
+        res.status(500).json({ message: "コンテンツの追加に失敗しました。"})
+    }
+}
