a798cff99aef2c63daa63b22168679f016bbab82
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-30 13:38:31 +0900

    チャプターのコンテンツを編集するAPIの作成

--- server/api-routes/contents.mjs
+++ server/api-routes/contents.mjs
@@ -1,9 +1,11 @@
 import express from "express"
-import { addContents, getAllContents } from "../controllers/contentsController.mjs";
+import { addContents, getAllContents, updateContents } from "../controllers/contentsController.mjs";
 
 const contentsRouter = express.Router();
 
 contentsRouter.get("/:id/:chapterId", getAllContents)
 contentsRouter.post("/:id/:chapterId", addContents)
+contentsRouter.patch("/:id/:chapterId/:contentsId", updateContents)
+
 
 export default contentsRouter;
--- server/controllers/contentsController.mjs
+++ server/controllers/contentsController.mjs
@@ -1,51 +1,91 @@
 import { findChapterById } from "../helpers/findChapterById.mjs";
 
 export const getAllContents = async (req, res) => {
-    try {
-        const bookId = req.params.id
-        const chapterId = req.params.chapterId
-    
-        const { chapter, error } = await findChapterById(bookId, chapterId)
+  try {
+    const bookId = req.params.id;
+    const chapterId = req.params.chapterId;
 
-        if (error) {
-            res.status(404).json({ message: error })
-        }
+    const { chapter, error } = await findChapterById(bookId, chapterId);
 
-        const contents =  chapter.contents
-        res.json(contents)
-    } catch (err) {
-        console.error("コンテンツの取得に失敗しました。", err);
-        res.status(500).json({ message: "コンテンツの取得に失敗しました。"})
+    if (error) {
+      res.status(404).json({ message: error });
     }
-}
+
+    const contents = chapter.contents;
+    res.json(contents);
+  } catch (err) {
+    console.error("コンテンツの取得に失敗しました。", err);
+    res.status(500).json({ message: "コンテンツの取得に失敗しました。" });
+  }
+};
 
 export const addContents = async (req, res) => {
-    try {
-        const bookId = req.params.id
-        const chapterId = req.params.chapterId
-    
-        const { chapter, book, error } = await findChapterById(bookId, chapterId)
-    
-        if (error) {
-            res.status(404).json({ message: error })
-        }
-    
-        const { heading_title, content } = req.body;
-    
-        if (!heading_title || !content) {
-          return res.status(400).json({ message: "heading_titleとcontentは必須です。" });
-        }
-    
-        const newContents = {
-          heading_title,
-          content,
-        };
-    
-        chapter.contents.push(newContents);
-        await book.save();
-        res.status(201).json(newContents)
-    } catch (err) {
-        console.error("コンテンツの追加に失敗しました。", err);
-        res.status(500).json({ message: "コンテンツの追加に失敗しました。"})
+  try {
+    const bookId = req.params.id;
+    const chapterId = req.params.chapterId;
+
+    const { chapter, book, error } = await findChapterById(bookId, chapterId);
+
+    if (error) {
+      res.status(404).json({ message: error });
     }
-}
+
+    const { heading_title, content } = req.body;
+
+    if (!heading_title || !content) {
+      return res
+        .status(400)
+        .json({ message: "heading_titleとcontentは必須です。" });
+    }
+
+    const newContents = {
+      heading_title,
+      content,
+    };
+
+    chapter.contents.push(newContents);
+    await book.save();
+    res.status(201).json(newContents);
+  } catch (err) {
+    console.error("コンテンツの追加に失敗しました。", err);
+    res.status(500).json({ message: "コンテンツの追加に失敗しました。" });
+  }
+};
+
+export const updateContents = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const chapterId = req.params.chapterId;
+
+    const { chapter, book, error } = await findChapterById(bookId, chapterId);
+
+    if (error) {
+      res.status(404).json({ message: error });
+    }
+
+    const contentsId = req.params.contentsId;
+    const contents = chapter.contents.find(
+      (content) => String(content._id) === contentsId
+    );
+
+    if (!contents) {
+      return res.status(404).json({ message: "コンテンツが見つかりません。" });
+    }
+
+    const { heading_title, content } = req.body;
+
+    if (heading_title !== undefined) {
+      contents.heading_title = heading_title;
+    }
+
+    if (content !== undefined) {
+      contents.content = content;
+    }
+
+    await book.save();
+    res.status(200).json(contents);
+  } catch (err) {
+    console.error("コンテンツの編集に失敗しました。", err);
+    res.status(500).json({ message: "コンテンツの編集に失敗しました。" });
+  }
+};
