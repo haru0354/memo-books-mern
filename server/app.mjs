fbb6a77781a642ce92e2481fbc6534b199a99eaa
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-30 14:56:30 +0900

    CORSの設定を追加

--- server/app.mjs
+++ server/app.mjs
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import "./helpers/db.mjs";
 import apiRoutes from "./api-routes/index.mjs"
+import cors from "cors"
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -11,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use('/api', apiRoutes)
+
+app.use(cors({
+  origin: "http://localhost:5173/",
+}))
 
 app.listen(port, () => {
   console.log(`サーバーが起動しました: http://localhost:${port}`);
