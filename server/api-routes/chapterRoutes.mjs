8d3231961154399e0bc2d6ec6d16e048d3457da8
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-31 02:52:30 +0900

    チャプターにエラーハンドラーを適用

--- server/api-routes/chapterRoutes.mjs
+++ server/api-routes/chapterRoutes.mjs
@@ -7,21 +7,26 @@ import {
   updateChapter,
 } from "../controllers/chapterController.mjs";
 import { body } from "express-validator";
+import { requestErrorHandler } from "../helpers/requestErrorHandler.mjs";
 
 const chapterRouter = express.Router();
 
-chapterRouter.get("/:id", getAllChapters);
-chapterRouter.get("/:id/:chapterId", getChapter);
+chapterRouter.get("/:id", requestErrorHandler(getAllChapters));
+
+chapterRouter.get("/:id/:chapterId", requestErrorHandler(getChapter));
+
 chapterRouter.post(
   "/:id/",
   body("chapter_title").notEmpty().withMessage("タイトルを入力してください。"),
-  addChapter
+  requestErrorHandler(addChapter)
 );
+
 chapterRouter.patch(
   "/:id/:chapterId",
   body("chapter_title").notEmpty().withMessage("タイトルを入力してください。"),
-  updateChapter
+  requestErrorHandler(updateChapter)
 );
-chapterRouter.delete("/:id/:chapterId", deleteChapter);
+
+chapterRouter.delete("/:id/:chapterId", requestErrorHandler(deleteChapter));
 
 export default chapterRouter;
