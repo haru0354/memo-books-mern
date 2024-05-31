f84bb1ad2fd38563289b77227ca50e715b97eb9c
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-31 16:35:55 +0900

    topぺージへのルーティングの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,16 @@
-import { useState } from "react";
 import "./App.css";
-import Button from "./components/ui/Button";
-import TextInput from "./components/ui/TextInput";
-import Textarea from "./components/ui/Textarea";
-import Footer from "./components/Footer";
-import DeleteModal from "./components/DeleteModal";
-import AddModal from "./components/AddModal";
-import ChapterList from "./components/ChapterList";
-import ContentsArea from "./components/ContentsArea";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer"
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const chapter = {
-    chapter_title: "チャプタータイトル",
-    contents: [
-      {
-        heading_title: "aa",
-        content: "00",
-      },
-      {
-        heading_title: "bb",
-        content: "11",
-      },
-      {
-        heading_title: "cc",
-        content: "22",
-      },
-    ],
-  };
-
-  const chapters = [
-    {
-      id: "0",
-      chapter_title: "aaa",
-    },
-    {
-      id: "1",
-      chapter_title: "bbb",
-    },
-  ];
-
   return (
-    <>
-      <h1>Vite00 + React</h1>
-      <ChapterList chapters={chapters} />
-      <ContentsArea chapter={chapter} />
-      <DeleteModal title="b" />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
