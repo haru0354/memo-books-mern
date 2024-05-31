ea816810851efdf1b1189896b182b3468b0bb7f7
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-31 16:39:25 +0900

    404ページを作成してルーティング

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Page404 />} />
       </Routes>
       <Footer />
     </BrowserRouter>
