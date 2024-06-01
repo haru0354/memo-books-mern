9d2e7b278ad00fbdf9741dc8e98936c660d191d5
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-06-02 00:11:42 +0900

    本の詳細ページのデザインの作成

---
+++ src/lib/formatDate.js
@@ -0,0 +1,11 @@
+// mongoDBのISO形式の日付を変換
+
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+
+  // 月は0から始まるため、+1する
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+  return `${year}-${month}-${day}`;
+};
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import bookApi from "../api/book";
 import ChapterList from "../components/ChapterList";
+import { RightContent, main2ColumnStyle } from "../styles/styles";
+import { formatDate } from "../lib/formatDate";
+import { css } from "@emotion/react";
+
+const h2Style = css`
+  margin-top: 4rem;
+  margin-bottom: 2rem;
+`;
 
 const Book = () => {
   const [book, setBook] = useState();
@@ -24,15 +32,20 @@ const Book = () => {
     return <p>Loading...</p>;
   }
 
+  const formattedCreatedDate = formatDate(book.createdAt);
+  const formattedUpdatedDate = formatDate(book.updatedAt);
+
   return (
-    <>
-      <div>本の詳細ページ(book)</div>
-      <h1>{book.title}</h1>
-      {book.description && (
-        <p>{book.description}</p>
-      )}
+    <main css={main2ColumnStyle}>
       <ChapterList chapters={book.chapters} bookId={book._id} />
-    </>
+      <div css={RightContent}>
+        <h1>{book.title}</h1>
+        <p>本の作成日：{formattedCreatedDate}</p>
+        <p>最終更新日：{formattedUpdatedDate}</p>
+        <h2 css={h2Style}>本の詳細</h2>
+        {book.description}
+      </div>
+    </main>
   );
 };
 
