81ecebcb9aca887a3ad14c9a00a3d01d702a5123
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-06-03 19:15:46 +0900

    コンテンツをReduxのstoreに追加

---
+++ src/store/slice/contentsSlice.js
@@ -0,0 +1,61 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const contentsSlice = createSlice({
+  name: "contents",
+  initialState: {
+    contents: [],
+    status: "idle",
+    error: null,
+  },
+  reducer: {
+    addContents(state, action) {
+      state.contents.push(action.payload);
+    },
+    updateContents(state, action) {
+      const { _id, heading_title, content } = action.payload;
+      const updateContents = state.contents.find((content) => {
+        content._id === _id;
+      });
+      if (updateContents) {
+        updateContents.heading_title = heading_title;
+        updateContents.content = content;
+      }
+    },
+    deleteContent(state, action) {
+      state.contents = state.contents.filter((content) => {
+        content._id !== action.payload._id;
+      });
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContents.pending, (state) => {
+        state.status = "Loading";
+      })
+      .addCase(fetchContents.fulfilled, (state) => {
+        state.status = "succeeded";
+        state.contents = action.payload;
+      })
+      .addCase(fetchContents.rejected, (state) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { addContents, deleteContent, updateContents } = contents.action;
+
+export const fetchContents = createAsyncThunk(
+  "contents/fetchContents",
+  async (bookId, chapterId) => {
+    try {
+      const data = await contentApi.getAll(bookId, chapterId);
+      return data;
+    } catch (error) {
+      console.error("コンテンツのフェッチに失敗しました");
+      throw error;
+    }
+  }
+);
+
+export default contentsSlice;
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./slice/booksSlice";
 import chaptersReducer from "./slice/chaptersSlice"
+import contentsReducer from "./slice/contentsSlice"
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     chapters: chaptersReducer,
+    contents: contentsReducer,
   },
 });
 
