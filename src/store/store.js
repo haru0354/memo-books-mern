3c1e11c90848818a9c1edd46a0f43c906279a1a4
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-06-09 17:51:39 +0900

    認証をチェックする状態管理をReduxに追加

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./slice/booksSlice";
 import chaptersReducer from "./slice/chaptersSlice"
 import contentsReducer from "./slice/contentsSlice"
+import authReducer from "./slice/authSlice"
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     chapters: chaptersReducer,
     contents: contentsReducer,
+    auth: authReducer,
   },
 });
 
