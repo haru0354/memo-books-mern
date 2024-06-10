b8c34fc4d8e1ce04b45c4396f17b1ba131829a0a
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-06-10 16:14:35 +0900

    バックのエンドポイントとフロントのAPIへのアクセスにuserIdを追加

--- src/api/content.js
+++ src/api/content.js
@@ -4,33 +4,33 @@ const expressUrl = import.meta.env.VITE_EXPRESS_URL;
 const ENDPOINT_URL = `${expressUrl}contents`;
 
 const contentApi = {
-  async getAll(bookId, chapterId) {
-    const result = await axios.get(`${ENDPOINT_URL}/${bookId}/${chapterId} `);
+  async getAll(userId, bookId, chapterId) {
+    const result = await axios.get(`${ENDPOINT_URL}/${userId}/${bookId}/${chapterId} `);
     return result.data;
   },
-  async get(bookId, chapterId, contentsId) {
+  async get(userId, bookId, chapterId, contentsId) {
     const result = await axios.get(
-      `${ENDPOINT_URL}/${bookId}/${chapterId}/${contentsId}`
+      `${ENDPOINT_URL}/${userId}/${bookId}/${chapterId}/${contentsId}`
     );
     return result.data;
   },
-  async post(bookId, chapterId, contents) {
+  async post(userId, bookId, chapterId, contents) {
     const result = await axios.post(
-      `${ENDPOINT_URL}/${bookId}/${chapterId}`,
+      `${ENDPOINT_URL}/${userId}/${bookId}/${chapterId}`,
       contents
     );
     return result.data;
   },
-  async patch(bookId, chapterId, contentsId, contents) {
+  async patch(userId, bookId, chapterId, contentsId, contents) {
     const result = await axios.patch(
-      `${ENDPOINT_URL}/${bookId}/${chapterId}/${contentsId}`,
+      `${ENDPOINT_URL}/${userId}/${bookId}/${chapterId}/${contentsId}`,
       contents
     );
     return result.data;
   },
-  async delete(bookId, chapterId, contentsId) {
+  async delete(userId, bookId, chapterId, contentsId) {
     const result = await axios.delete(
-      `${ENDPOINT_URL}/${bookId}/${chapterId}/${contentsId}`
+      `${ENDPOINT_URL}/${userId}/${bookId}/${chapterId}/${contentsId}`
     );
     return result.data;
   },
